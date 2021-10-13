bda85fef5268d50729a2d3d88a75ff5eb3216ec1
Keira <KeiraJCoder@hotmail.com>
2021-10-13 11:43:48 +0100

    changes

--- age_check.py
+++ age_check.py
@@ -1,3 +1,5 @@
+import datetime
+
 def player_age():
     while True:
         Age = input("How old are you? ")
@@ -12,3 +14,36 @@ def player_age():
             print("Amount must be a number, try again")
     return checker
 player_age()
+
+
+
+#  Variables & Big Catching
+age = input('What year where you born? ')
+if age.isdigit() is False:
+    print('Please enter a number')
+    quit()
+elif len(age) != 4:
+    print('Please enter a valid year!')
+    quit()
+elif age.isdigit() is True and len(age) == 4:
+    int_age = int(age)
+current_time = datetime.datetime.now().year
+print('You are', current_time - int_age, 'Years old')
+
+
+
+ 
+print("##### Welcome to age calculator ######")
+birth_year = int(input("Enter your year of birth: \n"))
+birth_month = int(input("Enter your month of birth: \n"))
+birth_day = int(input("Enter your day of birth: \n"))
+ 
+current_year = datetime.date.today().year
+current_month = datetime.date.today().month
+current_day = datetime.date.today().day
+ 
+age_year = current_year - birth_year
+age_month = abs(current_month-birth_month)
+age_day = abs(current_day-birth_day)
+ 
+print("Your exact age is: ", age_year, "Years", age_month, "months and", age_day, "days")
